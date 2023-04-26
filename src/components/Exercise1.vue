08a19a1c2186af53d4fba5c9393fec2e1776aef9
Edwin <miguepinchao1992@gmail.com>
2023-04-26 10:13:52 -0500

    Mi first component

---
+++ src/components/Exercise1.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const color= 'red';
+
+</script>
+
+<template>
+    <div>
+        <h1>My first component!</h1>
+        <span> {{ color }}</span>
+    </div>
+</template>
+
+<style>
+h1 {
+    font-family: 'Avenir', Helvetica, Arial,
+        sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
+
---
+++ src/views/MyFirstComponentView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Exercise1 from '../components/Exercise1.vue'
+</script>
+
+<template> 
+  <main>
+    <Exercise1 />
+  </main>
+</template>
